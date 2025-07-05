1b4fae233d3eee93c3b6404108a11b4a647cfd9a
Sejme <graumann.marius@gmail.com>
2025-07-05 20:03:25 +0200

    Movment adjusted to grid. Not working!

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,21 +1,28 @@
+using System;
 using System.Text.RegularExpressions;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Entity : MonoBehaviour
 {
-    private string entityName;
+    private string entityName = "Point";
     [SerializeField] private float moveSpeed = 5f;
-    private int remainingSteps = 3;
-    private float t = 0;
+    public int remainingSteps = 3;
+    public float t = 0;
     [SerializeField] private EntityState state;
     public Rigidbody2D rb;
     public Vector2 target;
+    private Vector2 oldPosition;
+    private Vector2Int PositionOnGrid = new Vector2Int(0,0);
+    private Direction facedDirection = Direction.Up;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb.position = GridRenderer.instance.TileToWorldPosition(PositionOnGrid);
+        startTurn();
     }
 
     // Update is called once per frame
@@ -35,14 +42,23 @@ public class Entity : MonoBehaviour
         }
     }
 
+    private void startTurn()
+    {
+        state = EntityState.Move;
+        GetNextTarget();
+    }
+
     private void MoveUpdate()
     {
-        t += moveSpeed * Time.fixedDeltaTime;
-        if (t > 1)
+        t += moveSpeed * Time.deltaTime;
+        if (t >= 1)
         {
             OnTargetReached();
         }
-        rb.MovePosition(Vector2.Lerp(rb.position, target, t));
+        else
+        {
+            rb.MovePosition(Vector2.Lerp(oldPosition, target, t));
+        }
     }
 
     private void IdleUpdate()
@@ -53,23 +69,118 @@ public class Entity : MonoBehaviour
     private void OnTargetReached()
     {
         t = 0;
-        remainingSteps += -1;
+        remainingSteps -= 1;
         if (remainingSteps <= 0)
         {
             TurnController.instance.NpcMovementFinished();
-
+            state = EntityState.Idle;
         }
         else
         {
-            target = GetNextTarget();
+            GetNextTarget();
         }
     }
-
-    private Vector2 GetNextTarget()
+    private void GetNextTarget()
     {
-        float x = Random.Range(-2f, 2f);
-        float y = Random.Range(-2f, 2f);
-        return new Vector2(x, y);
+        Vector2Int targetTile = GetNextTargetCoord();
+        Vector3 target3D = GridRenderer.instance.TileToWorldPosition(targetTile);
+        target = new Vector2(target3D.x, target3D.y);
+        oldPosition = rb.position;
+        Debug.Log("Set new target to: "+target.ToString());
+    }
+
+    private Vector2Int GetNextTargetCoord()
+    {
+        float rand = 0;
+        Direction[] directionsToCheck = { facedDirection, GetLeftDirection(facedDirection), GetRightDirection(facedDirection), LevelGrid.GetOppositeDirection(facedDirection) };
+        if (rand < 0.5)
+        {
+            directionsToCheck[2] = GetLeftDirection(facedDirection);
+            directionsToCheck[1] = GetRightDirection(facedDirection);
+        }
+        for (int i = 0; i < directionsToCheck.Length; i++)
+        {
+            if (LevelGrid.instance.IsDirectionFree(PositionOnGrid.x, PositionOnGrid.y, directionsToCheck[i]))
+            {
+                //get neighbour coords
+                int x = PositionOnGrid.x;
+                int y = PositionOnGrid.y;
+                int neighborX = x, neighborY = y;
+                switch (directionsToCheck[i])
+                {
+                    case Direction.Up:
+                        neighborY = y - 1;
+                        break;
+                    case Direction.Down:
+                        neighborY = y + 1;
+                        break;
+                    case Direction.Left:
+                        neighborX = x - 1;
+                        break;
+                    case Direction.Right:
+                        neighborX = x + 1;
+                        break;
+                }
+                Debug.Log(entityName + " has CHoosen direction " + directionsToCheck[i]);
+                Debug.Log("Target is at pos("+neighborX+ ", " + neighborY + ")");
+                Face(directionsToCheck[i]);
+                return new Vector2Int(neighborX, neighborY);
+            }
+        }
+        return new Vector2Int(0,0);
+    }
+
+    private void Face(Direction direction)
+    {
+        Debug.Log("Facing direction" + direction);
+        facedDirection = direction;
+        switch (direction)
+        {
+            case Direction.Up:
+                this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case Direction.Left:
+                this.transform.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case Direction.Down:
+                this.transform.rotation = Quaternion.Euler(0, 0, 180);
+                break;
+            case Direction.Right:
+                this.transform.rotation = Quaternion.Euler(0, 0, 270);
+                break;
+        }
+    }
+    public static Direction GetLeftDirection(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return Direction.Left;
+            case Direction.Down:
+                return Direction.Right;
+            case Direction.Left:
+                return Direction.Down;
+            case Direction.Right:
+                return Direction.Up;
+            default:
+                return Direction.Up;
+        }
+    }
+    public static Direction GetRightDirection(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return Direction.Right;
+            case Direction.Down:
+                return Direction.Left;
+            case Direction.Left:
+                return Direction.Up;
+            case Direction.Right:
+                return Direction.Down;
+            default:
+                return Direction.Up;
+        }
     }
 }
 
