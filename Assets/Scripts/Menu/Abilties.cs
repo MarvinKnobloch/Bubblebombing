9a2e6fd5ec32d8f6e9cf5b9f6a3c6f7ee14e4b72
MarvinKnobloch <NivramKnobloch@gmail.com>
2025-07-05 18:43:09 +0200

    Ability stuff

--- Assets/Scripts/Menu/Abilties.cs
+++ Assets/Scripts/Menu/Abilties.cs
@@ -3,9 +3,38 @@ using UnityEngine.InputSystem;
 
 public class Abilties : MonoBehaviour
 {
+    private Controls controls;
+
     [SerializeField] private GameObject boostpadPreview;
-    public void CreatBoostpadPreview()
+    [SerializeField] private GameObject slowPadPreview;
+
+    private GameObject currentPreview;
+
+    private void Awake()
     {
-        Instantiate(boostpadPreview, Mouse.current.position.ReadValue(), Quaternion.identity);  
+        controls = new Controls();
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void Update()
+    {
+        //if(controls.Player)
+    }
+
+    public void CreateBoostpadPreview()
+    {
+        CurrentPreviewDestroy();
+       currentPreview = Instantiate(boostpadPreview, Mouse.current.position.ReadValue(), Quaternion.identity);  
+    }
+    public void CreateSlowPadPreview()
+    {
+        CurrentPreviewDestroy();
+        currentPreview = Instantiate(slowPadPreview, Mouse.current.position.ReadValue(), Quaternion.identity);
+    }
+    private void CurrentPreviewDestroy()
+    {
+        if (currentPreview != null) Destroy(currentPreview);
     }
 }
