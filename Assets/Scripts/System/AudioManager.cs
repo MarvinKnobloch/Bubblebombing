ca737c1e1c78e8e10f78689e5962989d0ddbe814
MarvinKnobloch <NivramKnobloch@gmail.com>
2025-07-07 14:36:41 +0200

    AudioManager

--- Assets/Scripts/System/AudioManager.cs
+++ Assets/Scripts/System/AudioManager.cs
@@ -1,16 +1,85 @@
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static AudioManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource soundSource;
+
+    [Space]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private string masterVolume;
+    [SerializeField] private string musicVolume;
+    [SerializeField] private string soundVolume;
+
+    public enum MusicSongs
     {
-        
+        Empty,
+        Menu,
+        Level,
+    }
+    public enum UtilitySounds
+    {
+        Empty,
+        MenuSelect,
+        CurrencyGain,
+    }
+    public enum PlayerSounds
+    {
+        Empty,
+        PlayerDeath,
+    }
+    public enum EnemySounds
+    {
+        Empty
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("AudioHasBeenChange") == 0)
+        {
+            PlayerPrefs.SetFloat("SliderValue" + masterVolume, 0.8f);
+            PlayerPrefs.SetFloat(masterVolume, Mathf.Log10(PlayerPrefs.GetFloat("SliderValue" + masterVolume) * 20));
+            SetVolume(masterVolume, 0);
+
+            PlayerPrefs.SetFloat("SliderValue" + musicVolume, 0.8f);
+            PlayerPrefs.SetFloat(musicVolume, Mathf.Log10(PlayerPrefs.GetFloat("SliderValue" + musicVolume) * 20));
+            SetVolume(musicVolume, 0);
+
+            PlayerPrefs.SetFloat("SliderValue" + soundVolume, 2.4f);
+            PlayerPrefs.SetFloat(soundVolume, Mathf.Log10(PlayerPrefs.GetFloat("SliderValue" + soundVolume) * 20));
+            SetVolume(soundVolume, 0);
+        }
+        else
+        {
+            SetVolume(masterVolume, 0);
+            SetVolume(musicVolume, 0);
+            SetVolume(soundVolume, 10);
+        }
+
+    }
+    private void SetVolume(string volumename, float maxdb)
+    {
+        audioMixer.SetFloat(volumename, PlayerPrefs.GetFloat(volumename));
+        bool gotvalue = audioMixer.GetFloat(volumename, out float soundvalue);
+        if (gotvalue == true)
+        {
+            if (soundvalue > maxdb)
+            {
+                audioMixer.SetFloat(volumename, maxdb);
+            }
+        }
     }
 }
